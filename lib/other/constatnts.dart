a3dce6205c7a0d96a7beb7502a9aeb6db17600b7
KumarSunil17 <42208638+KumarSunil17@users.noreply.github.com>
2019-02-08 13:10:18 +0530

    Initial commit

---
+++ lib/other/constatnts.dart
@@ -0,0 +1,6 @@
+class Constants{
+  static final String WEBSITE="http://nandighoshfoodnservices.com/";
+  static final String API_URL=WEBSITE+"api/";
+  static final String GALLERY_API="getAllPhotos.php";
+  static final String GALLERY_URL=WEBSITE+"gallery/";
+}
